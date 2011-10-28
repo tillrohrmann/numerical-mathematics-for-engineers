1d63fa0cb5ca3f0399206d311a81578bc5ca413a
Till Rohrmann <rohrmann@nb-rohrmann.(none)>
2011-10-28 12:40:13 +0200

    homework 2 created

--- exercises/homework1/p02gs.m
+++ exercises/homework1/p02gs.m
@@ -1,7 +1,8 @@
 function[U] = p02gs(V)
 % Programming Exercise Numerical Mathematics for Engineers II
 % Homework 1
-% Authors: Sophie Kohle (MatrNr.: ), Ana Kosareva (MatrNr.: ), Till Rohrmann (MatrNr.: 343756)
+% Group: nm2-103
+% Authors: Sophia Kohle (MatrNr.: 341322), Ana Kosareva (MatrNr.: 321785), Till Rohrmann (MatrNr.: 343756)
 % 
 % Using OCTAVE
 	for i=1:1:columns(V)
